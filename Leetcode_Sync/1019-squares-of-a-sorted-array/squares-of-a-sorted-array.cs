477c1555f93a6df58ad020efc333c8a65ab222b9
Vivek Bagane <vivoo.thecoder@gmail.com>
2026-07-29 23:13:03 +0530

    Time: 2 ms (45.66%) | Memory: 65.3 MB (5.93%) - LeetSync

---
+++ Leetcode_Sync/1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public int[] SortedSquares(int[] nums) {
+        int [] result = new int[nums.Length]; 
+        int i = 0,
+            j = nums.Length-1;
+
+        int k = j;
+
+        //[-3, 2, 4]
+        // i
+        //         j
+        //         k     
+
+        while(i<= j)
+        {
+            if(Math.Abs(nums[i]) > Math.Abs(nums[j]))
+            {
+                result[k] = nums[i] * nums[i];
+                i++;
+            }else
+            {
+                result[k] = nums[j] * nums[j];
+                j--;
+            }
+
+            k--;
+        } 
+
+        return result;  
+    }
+}
