bc940fc0a4bbe221535aad6da3c3dc989c7339af
Vivek Bagane <vivoo.thecoder@gmail.com>
2026-07-29 23:26:00 +0530

    #explanation added

--- Leetcode_Sync/1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cs
+++ Leetcode_Sync/1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cs
@@ -1,31 +1,43 @@
 public class Solution {
     public int[] SortedSquares(int[] nums) {
-        int [] result = new int[nums.Length]; 
+        // Create a result array of the same length as nums
+        int [] result = new int [nums.Length];
+ 
+        // i points to the start (left side)
         int i = 0,
+            // j points to the end (right side)
             j = nums.Length-1;
-
-        int k = j;
-
-        //[-3, 2, 4]
-        // i
-        //         j
-        //         k     
-
-        while(i<= j)
-        {
+ 
+        // k points to the last index of result (we fill from the back)
+        int k = nums.Length-1;
+ 
+        // Loop until both pointers meet
+        while(i<=j)
+        {    
+            // Why compare absolute values?
+            // Because the array is sorted, but negative numbers can have larger squares.
+            // Example: -7 squared (49) is bigger than 3 squared (9).
+            // So we must check which side has the bigger absolute value.
+           
             if(Math.Abs(nums[i]) > Math.Abs(nums[j]))
             {
+                // If left side has bigger absolute value, square it
                 result[k] = nums[i] * nums[i];
+                // Move left pointer forward
                 i++;
             }else
             {
+                // Otherwise, square the right side
                 result[k] = nums[j] * nums[j];
+                // Move right pointer backward
                 j--;
             }
-
+ 
+            // Move k backward (fill result from end to start)
             k--;
-        } 
-
-        return result;  
+        }  
+ 
+        // Return the sorted squares
+        return result;
     }
 }
