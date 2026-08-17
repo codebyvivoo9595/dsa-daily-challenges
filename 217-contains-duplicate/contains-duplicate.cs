e935e834c17f11ddbea2bd08fe1d5de29952f3b0
Vivek Bagane <vivoo.thecoder@gmail.com>
2026-08-17 23:33:53 +0530

    Time: 15 ms (63.19%) | Memory: 78 MB (32.50%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+      HashSet<int> hs = new HashSet<int>();
+      
+      foreach(var num in nums)
+      {
+        if(!hs.Add(num))
+        {
+           return true;
+        }
+      }
+
+      return false;
+
+
+    }
+}
