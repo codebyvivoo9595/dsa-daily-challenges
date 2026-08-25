9365d1a0bae212b8d0a7b419de301de64e03f13b
Vivek Bagane <vivoo.thecoder@gmail.com>
2026-08-25 23:07:56 +0530

    #Left Rotate by K elements - dev

---
+++ Striver-Playlist/Medium_Left_Rotate_Array_by_K_Place.cs
@@ -0,0 +1,34 @@
+public class Solution {
+    public int[] HelperLeftOneRotationOfArray(int[] nums)
+        {
+           int firstPlace = nums[0]; 
+           int n = nums.Length; 
+           for(int i = 1; i < n ; i++)
+           {
+              //That value should be place in i-1 position.
+              nums[i-1] = nums[i];
+              //This will Traverse the array and set the value in i-1 location
+           }
+
+           //Lastly last value we need to add that value is in n-1 location so
+           nums[n-1] = firstPlace;
+           return nums;
+        } 
+
+    public void Rotate(int[] nums, int k) {
+        // See what i can do i know the one left rotation 
+        // let suppose they are asking rotate array 3 time 
+        //so i will that function 3 time 
+        //So array will rotate 3 times
+
+    
+        //Now comes the main part 
+        //How many time i need to rotate array is k
+
+        for (int i = 0 ; i < k; i++)
+        {
+           nums = HelperOneRotationOfArray(nums);
+        }
+
+    }
+}
